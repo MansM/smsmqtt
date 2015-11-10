c1b8be72a8f830f9743fdba5b2f83062394ab0eb
Mans Matulewicz <mans.matulewicz@gmail.com>
2015-11-10 14:53:08 +0100

    broker now from get

--- src/index.php
+++ src/index.php
@@ -1,11 +1,12 @@
 <?php
 require("phpMQTT.php");
 
+$broker = $_GET['broker'];
 $sender = $_GET['sender'];
 $topic = $_GET['topic'];
 $message = $_GET['payload'];
 
-$mqtt = new phpMQTT("10.0.1.52", 1883, $sender); //Change client name to something unique
+$mqtt = new phpMQTT($broker, 1883, $sender); //Change client name to something unique
 if ($mqtt->connect()) {
 	$mqtt->publish($topic, $message,0);
 	$mqtt->close();
